d1db14f36311e3fc1c934a865a08f3dfad082546
khovan <minhpnq1807@gmail.com>
2023-12-03 19:04:34 +0700

    up button 3

---
+++ button/button_3/app.js
@@ -0,0 +1,30 @@
+import { images } from "./data.js";
+console.log(images);
+let index = 0;
+document.querySelector(".btn-next").addEventListener("click", function (e) {
+  if (index === images.length) return;
+  index = index + 1;
+  const currentSrc = document
+    .querySelector(".image-content .image")
+    .setAttribute("src", images[index]);
+  document.querySelector(".image-numbers").textContent =
+    index + 1 + " of " + images.length;
+});
+
+document.querySelector(".btn-back").addEventListener("click", function (e) {
+  if (index === 0) return;
+  index = index - 1;
+  const currentSrc = document
+    .querySelector(".image-content .image")
+    .setAttribute("src", images[index]);
+  document.querySelector(".image-numbers").textContent =
+    index + 1 + " of " + images.length;
+});
+document.querySelector(".btn-add").addEventListener("click", function (e) {
+  const imageURL = prompt("Enter the URL of the image:");
+  if (imageURL) {
+    images.push(imageURL);
+    document.querySelector(".image-numbers").textContent =
+      index + 1 + " of " + images.length;
+  }
+});
---
+++ button/button_3/data.js
@@ -0,0 +1,7 @@
+export const images = [
+  `https://images.unsplash.com/photo-1700994630045-f7a20df6d92e?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D`,
+  `https://images.unsplash.com/photo-1701198067264-0d9b607bd399?q=80&w=2509&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D`,
+  `https://plus.unsplash.com/premium_photo-1700984292461-fa2d83c28c6b?q=80&w=2572&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D`,
+  `https://images.unsplash.com/photo-1701352281350-ed4a1e76c303?q=80&w=2535&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D`,
+];
+export default images;
