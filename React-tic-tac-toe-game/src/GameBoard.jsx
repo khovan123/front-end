6c9d21032c9c3454982771dc636e83e207964b7d
khovan <minhpnq1807@gmail.com>
2023-12-06 15:21:39 +0700

    change state in gameboard and up state in app

---
+++ React-tic-tac-toe-game/src/GameBoard.jsx
@@ -0,0 +1,43 @@
+import "./index.css";
+const intitialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+export default function GameBoard({ onSelectSquare, turns }) {
+  let gameBoard = [...intitialGameBoard];
+  for (const turn of turns) {
+    const { Square, Player } = turn;
+    const { row, col } = Square;
+    gameBoard[row][col] = Player;
+  }
+  // const [gameBoard, setGameBoard] = useState(intitialGameBoard);
+
+  // function handleSelectSquare(rowIndex, colIndex) {
+  //   setGameBoard((prevGameBoard) => {
+  //     const updatedGameBoard = [
+  //       ...prevGameBoard.map((innerArray) => [...innerArray]),
+  //     ];
+  //     updatedGameBoard[rowIndex][colIndex] = activePlayerSymbol;
+  //     return updatedGameBoard;
+  //   });
+  //   onSelectSquare();
+  // }
+  return (
+    <ol id="game-board">
+      {gameBoard.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((playerSymbol, colIndex) => (
+              <li key={colIndex}>
+                <button onClick={() => onSelectSquare(rowIndex, colIndex)}>
+                  {playerSymbol}
+                </button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
+    </ol>
+  );
+}
