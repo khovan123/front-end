41745a171b8f556919fd5dfa242220e62993dbd9
khovan <minhpnq1807@gmail.com>
2023-12-07 12:12:49 +0700

    updated Investment Caculator

---
+++ Investment-Caculator/src/components/Header.jsx
@@ -0,0 +1,9 @@
+import logo from "../assets/investment-calculator-logo.png";
+export default function Header() {
+  return (
+    <header id="header">
+      <img src={logo} alt="image"></img>
+      <h1>Investment Caculator</h1>
+    </header>
+  );
+}
