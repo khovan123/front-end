4a0e66e56f533c88f3ee9e2f90a854560e75e4cf
khovan123 <minhpnq1807@gmail.com>
2023-12-20 14:30:01 +0700

    update use custom Hook for LoginState and use default condition Input from browser

--- Form/src/components/Login.jsx
+++ Form/src/components/Login.jsx
@@ -3,10 +3,10 @@ import { useRef, useState } from "react";
 export default function Login() {
   const email = useRef();
   const password = useRef();
-  function handleSubmit(event){
+  function handleSubmit(event) {
     event.preventDefault();
-    console.log('email: ', email.current.value);
-    console.log('password: ', password.current.value);
+    console.log("email: ", email.current.value);
+    console.log("password: ", password.current.value);
   }
   return (
     <form onSubmit={handleSubmit}>
@@ -15,18 +15,37 @@ export default function Login() {
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" value={email.current} ref={email}/>
+          <input
+            id="email"
+            type="email"
+            name="email"
+            value={email.current}
+            ref={email}
+            required
+          />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" value={password.current} ref={password}/>
+          <input
+            id="password"
+            type="password"
+            name="password"
+            value={password.current}
+            ref={password}
+            required
+            minLength={6}
+          />
         </div>
       </div>
 
       <p className="form-actions">
-        <button className="button button-flat">Reset</button>
-        <button className="button">Login</button>
+        <button type="reset" className="button button-flat">
+          Reset
+        </button>
+        <button type="submit" className="button">
+          Login
+        </button>
       </p>
     </form>
   );
--- Form/src/components/LoginState.jsx
+++ Form/src/components/LoginState.jsx
@@ -1,39 +1,73 @@
-import { useState } from "react";
-
+import { isEmail, isNotEmpty, hasMinLength } from "../util/validation.js";
+import Input from "./Input.jsx";
+import { useInput } from "../hooks/useInput.js";
 export default function Login() {
-  const [userValue,setUserValue] = useState({
-    email:'',
-    password:'',
+  const {
+    value: emailValue,
+    handleBlur: handleEmailBlur,
+    handleChange: handleEmailChange,
+    hasError: emailHasError,
+  } = useInput("", (value) => {
+    return isEmail(value) && isNotEmpty(value);
   });
-  function handleChange(identifier,value){
-    setUserValue(prev=>({
-      ...prev,
-    [identifier]:value,
-    }))
-  }
-  function handleSubmit(event){
+
+  const {
+    value: passwordValue,
+    handleBlur: handlePasswordBlur,
+    handleChange: handlePasswordChange,
+    hasError: passwordHasError,
+  } = useInput("", (value) => {
+    return hasMinLength(value, 6);
+  });
+
+  function handleSubmit(event) {
     event.preventDefault();
-    console.log(userValue);
+    if (
+      emailHasError ||
+      passwordHasError ||
+      emailValue === "" ||
+      passwordValue === ""
+    ) {
+      return;
+    }
+    console.log(emailValue, passwordValue);
   }
+
   return (
     <form onSubmit={handleSubmit}>
       <h2>Login</h2>
 
       <div className="control-row">
-        <div className="control no-margin">
-          <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" value={userValue.email} onChange={(event)=>handleChange('email',event.target.value)}/>
-        </div>
+        <Input
+          label={"Email"}
+          id={"email"}
+          type="email"
+          name="email"
+          value={emailValue}
+          onChange={handleEmailChange}
+          onBlur={handleEmailBlur}
+          error={emailHasError && "Please enter a valid email."}
+        />
 
-        <div className="control no-margin">
-          <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" value={userValue.password} onChange={(event)=>handleChange('password',event.target.value)}/>
-        </div>
+        <Input
+          label={"Password"}
+          id={"password"}
+          type="password"
+          name="password"
+          value={passwordValue}
+          onChange={handlePasswordChange}
+          onBlur={handlePasswordBlur}
+          error={passwordHasError && "Please enter a valid password."}
+        />
       </div>
 
       <p className="form-actions">
-        <button className="button button-flat">Reset</button>
-        <button className="button">Login</button>
+        <button type="reset" className="button button-flat">
+          Reset
+        </button>
+        <button type="submit" className="button">
+          Login
+        </button>
       </p>
     </form>
   );
